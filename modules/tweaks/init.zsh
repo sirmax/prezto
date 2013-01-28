35a16c8fbfe603cbbdf1f6e8c376614c34e2eb78
Max Lanin <Max.M.Lanin@gmail.com>
2013-01-28 23:33:53 +0400

    Add "tweaks" module. Add aliases for easy JDK switching

---
+++ modules/tweaks/init.zsh
@@ -0,0 +1,10 @@
+#
+# Various tweaks in no particular order.
+#
+
+alias j6='JAVA_HOME=$(/usr/libexec/java_home -v 1.6)'
+alias j7='JAVA_HOME=$(/usr/libexec/java_home -v 1.7)'
+alias j8='JAVA_HOME=$(/usr/libexec/java_home -v 1.8)'
+alias j9='JAVA_HOME=$(/usr/libexec/java_home -v 9)'
+alias jlast='JAVA_HOME=$(/usr/libexec/java_home)'
+alias jversion='java -version'
