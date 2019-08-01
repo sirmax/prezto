217c659e7b715f465bf2afedcae86313c1f80e73
Maksim Lanin <mlanin@evolutiongaming.com>
2019-08-01 12:47:38 +0300

    Add j11 and gvm aliases

--- modules/tweaks/init.zsh
+++ modules/tweaks/init.zsh
@@ -7,5 +7,8 @@ alias j7='JAVA_HOME=$(/usr/libexec/java_home -v 1.7)'
 alias j8='JAVA_HOME=$(/usr/libexec/java_home -v 1.8)'
 alias j9='JAVA_HOME=$(/usr/libexec/java_home -v 9)'
 alias j10='JAVA_HOME=$(/usr/libexec/java_home -v 10)'
+alias j11='JAVA_HOME=$(/usr/libexec/java_home -v 11)'
 alias jlast='JAVA_HOME=$(/usr/libexec/java_home)'
 alias jversion='java -version'
+
+alias gvm='PATH=~/GraalVM/graalvm-ce-1.0.0-rc11/Contents/Home/bin:$PATH; JAVA_HOME=~/GraalVM/graalvm-ce-1.0.0-rc11/Contents/Home'
